e778444a25b28a90c919c8f5505e76b6c39124b3
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-25 08:24:13 +1000

    Update index.js

--- JS Projects/1.0 Begginer/Expense Tracker/index.js
+++ JS Projects/1.0 Begginer/Expense Tracker/index.js
@@ -25,29 +25,64 @@ function ExpenseCalculate() {
 const ExpenseList = document.getElementById("ExpenseList");
 const ExpenseUL = document.getElementById("ExpenseUL")
 
+ExpenseUL.style.padding = "0px"
+
 function syncfunction() {
-    const list = document.createElement("li")
-    list.style.backgroundColor = "red"
-    list.style.marginBottom = "10px"
-    list.style.height = "auto";
-    list.style.width = "95%"
-    list.style.position = "relative";
-    list.style.bottom = "5px"
+    if (BudgetInput.value !== "" && ExpenseInput.value !== "" && ExpenseAmountInput.value !== "") {
+        const list = document.createElement("li")
+        list.style.backgroundColor = "rgba(201, 201, 201, 0.8)"
+        // An RGB color value represents RED, GREEN, and BLUE light sources.
+        //  An RGBA color value is an extension of RGB with an Alpha channel (opacity).
+        list.style.marginBottom = "10px"
+        list.style.left = "3%"
+        list.style.height = "5vh";
+        list.style.width = "95%"
+        list.style.position = "relative";
+        list.style.bottom = "5px"
+        
+    
+        const p = document.createElement("p")
+        p.style.margin = "0px"
+        p.textContent = ExpenseInput.value
+        p.style.position = "relative";
+        p.style.left = "10%"
+        p.style.bottom = "60%"
 
-    const p = document.createElement("p")
-    p.textContent = ExpenseInput.value
 
-    const deletebutton = document.createElement("button")
-    deletebutton.style.backgroundColor = "blue"
+        const p2 = document.createElement("p")
+        p2.style.margin = "0px"
+        p2.textContent = ExpenseAmountInput.value
+        p2.style.position = "relative";
+        p2.style.left = "45%"
+        p2.style.bottom = "100%"
+    
+    
+        const imagesrc = document.createElement("img")
+        imagesrc.style.position = "relative"
+        imagesrc.style.left = "80%"
+        imagesrc.src = "Images/Trash.png"
+        imagesrc.style.height = "35px"
+        imagesrc.style.cursor = "pointer"
 
-    deletebutton.onclick = function() {
-        list.remove();
-        deletebutton.remove()
+        imagesrc.onclick = function() {
+            list.remove()
+            deletebutton.remove()
+        }
+
+        const imagesrc2 = document.createElement("img")
+        imagesrc2.style.position = "relative"
+        imagesrc2.src = "Images/Pen.png"
+        imagesrc2.style.height = "35px"
+        imagesrc2.style.left = "75%"
+        imagesrc2.style.cursor = "pointer"
+    
+        ExpenseUL.appendChild(list)
+        list.appendChild(imagesrc2)
+        list.appendChild(imagesrc)
+        list.appendChild(p)
+        list.appendChild(p2)
+        list.appendChild(deletebutton)
     }
-
-    ExpenseUL.appendChild(list)
-    list.appendChild(p)
-    list.appendChild(deletebutton)
 }
 
 
@@ -61,5 +96,3 @@ function syncfunction() {
 // text = localStorage.getItem("Text")
 
 // console.log(text)
-
-
