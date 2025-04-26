a30c8174e85aaedd43ccf9896f9d3ea59f9035a0
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-26 16:15:27 +1000

    Add files via upload

---
+++ JS Projects/1.0 Begginer/2.0 Intermediate/Tic Tac Toe/index.js
@@ -0,0 +1,20 @@
+
+
+
+function Restartfunction() {
+    // querySelector - the first element within the document that matches their specific css seletor
+    // document.querySelector('td').textContent = ""
+
+
+   if(document.getElementsByTagName("td") == true) {
+    alert("f")
+   }
+}
+
+
+
+
+const td = document.getElement("td")
+
+
+
