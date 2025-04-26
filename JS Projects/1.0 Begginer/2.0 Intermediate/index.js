ae6729f485fd1c3e4ea0dfdb6b843a573a9336e9
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-27 08:23:57 +1000

    Add files via upload

---
+++ JS Projects/1.0 Begginer/2.0 Intermediate/index.js
@@ -0,0 +1,13 @@
+
+
+function CreateNote() {
+    const Note = document.createElement("textarea")
+    Note.style.height = "auto"
+    Note.style.backgroundColor = "white"
+    document.body.appendChild(Note)
+}
+
+
+
+
+
