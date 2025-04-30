7ff325d26116eaa55d770bb53f061c4272c93991
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-30 18:59:23 +1000

    Update index.js

--- JS Projects/1.0 Begginer/2.0 Intermediate/Notes/index.js
+++ JS Projects/1.0 Begginer/2.0 Intermediate/Notes/index.js
@@ -1,20 +1,44 @@
-const NoteDiv = document.getElementById("NoteDiv")
+const NoteDiv = document.getElementById("NoteDiv");
 const AddNoteBTN = document.getElementById("AddNoteBTN");
+const TitleInput = document.getElementById("TitleInput");
+const DescriptionInput = document.getElementById("DescriptionInput");
+
+function OpenNote() {
+    NoteDiv.style.display = "block" // when you click always display block
+}
 
 function CreateNote() {
-    NoteDiv.style.display = "block"
-  const NewNote = document.createElement("div")
-  NewNote.style.borderRadius = "4px"
-  NewNote.style.height = "20vh";
-  NewNote.style.width = "20%"
-  NewNote.style.backgroundColor = "red"
-  document.body.appendChild(NewNote);
+    if (TitleInput.value != "" && DescriptionInput.value != "") {
+      const NewNote = document.createElement("div")
+      NewNote.style.position = "relative"
+      NewNote.style.borderRadius = "4px"
+      NewNote.style.height = "auto";
+      NewNote.style.width = "20%"
+      NewNote.style.backgroundColor = "white"
+
+      const TitleText = document.createElement("p")
+      TitleText.textContent = `${TitleInput.value}`
+      TitleText.style.fontWeight = "bold"
+
+      const DescriptionText = document.createElement("p")
+      DescriptionText.textContent = `${DescriptionInput.value}`
+      DescriptionText.style.marginBottom = "20px"
+
+      const line = document.createElement("div")
+      line.style.position = "relative"
+      line.style.top = "5%"
+      line.style.width = "100%"
+      line.style.backgroundColor = "green"
+      line.style.height = "20px"
+
+      NewNote.appendChild(TitleText)
+      NewNote.appendChild(line)
+      NewNote.appendChild(DescriptionText)
+      document.body.appendChild(NewNote);
+    }
 }
 
 
 function closeNote() {
-    
     NoteDiv.remove()
 }
-
-
