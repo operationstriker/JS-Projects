b5a05d19304b69dbb3dd80477be8bd305206820c
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-30 12:39:29 +1000

    Update index.js

--- JS Projects/1.0 Begginer/2.0 Intermediate/Notes/index.js
+++ JS Projects/1.0 Begginer/2.0 Intermediate/Notes/index.js
@@ -1,7 +1,14 @@
 const NoteDiv = document.getElementById("NoteDiv")
+const AddNoteBTN = document.getElementById("AddNoteBTN");
 
 function CreateNote() {
     NoteDiv.style.display = "block"
+  const NewNote = document.createElement("div")
+  NewNote.style.borderRadius = "4px"
+  NewNote.style.height = "20vh";
+  NewNote.style.width = "20%"
+  NewNote.style.backgroundColor = "red"
+  document.body.appendChild(NewNote);
 }
 
 
