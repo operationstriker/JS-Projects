e193633113fa2dae0d94461acb469c28c03044df
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-05-01 11:53:51 +1000

    Update index.js

--- JS Projects/1.0 Begginer/2.0 Intermediate/Notes/index.js
+++ JS Projects/1.0 Begginer/2.0 Intermediate/Notes/index.js
@@ -23,6 +23,7 @@ function CreateNote() {
       const DescriptionText = document.createElement("p")
       DescriptionText.textContent = `${DescriptionInput.value}`
       DescriptionText.style.marginBottom = "20px"
+      DescriptionText.style.bottom = "10px";
 
       const line = document.createElement("div")
       line.style.position = "relative"
@@ -31,6 +32,57 @@ function CreateNote() {
       line.style.backgroundColor = "green"
       line.style.height = "20px"
 
+      const date = new Date()
+      
+
+      const monthtext = document.createElement("p")
+      monthtext.textContent = date.getMonth()
+
+
+      switch(date.getMonth()) {
+        case 1:
+            monthtext.textContent = "January"
+            break;
+        case 2:
+            monthtext.textContent = "February"
+            break;
+        case 3:
+            monthtext.textContent = "March"
+            break;
+        case 4:
+            monthtext.textContent = "April"
+            break;
+        case 5:
+            monthtext.textContent = "May"
+            break;
+        case 6:
+            monthtext.textContent = "June"
+            break;
+        case 7:
+            monthtext.textContent = "July"
+            break;
+        case 8:
+            monthtext.textContent = "August"
+            break;
+        case 9:
+            monthtext.textContent = "September"
+            break;
+        case 10:
+            monthtext.textContent = "October"
+            break;
+        case 11:
+            monthtext.textContent = "November"
+            break;
+        case 12:
+            monthtext.textContent = "December"
+            break;    
+      }
+
+      const yeartext = document.createElement("p")
+      yeartext.textContent =  date.getFullYear() 
+      
+      NewNote.appendChild(monthtext)
+      NewNote.appendChild(yeartext);
       NewNote.appendChild(TitleText)
       NewNote.appendChild(line)
       NewNote.appendChild(DescriptionText)
