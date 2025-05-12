581dae61a1cda973d333b4de9b838704aa76d8ff
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-05-12 12:17:24 +1000

    Update index.js

--- JS Projects/Startups/Link in bio/index.js
+++ JS Projects/Startups/Link in bio/index.js
@@ -6,6 +6,7 @@ function getlink() {
     if (Input.value !== "") {
    const NewDiv = document.createElement("div")
    NewDiv.id = "NewDiv"
+  NewDiv.innerText = Input.value
    document.body.appendChild(NewDiv)
     }
 }
